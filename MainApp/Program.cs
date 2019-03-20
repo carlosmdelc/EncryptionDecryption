fd2af07dd9ec6ad3b893311c10c5d582765249c9
Carlos M <carlosmdelc@gmail.com>
2019-03-19 18:37:23 -0600

    RSA addition

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RivestShamirAdlemanRSA;
 
 namespace MainApp
 {
@@ -10,6 +11,10 @@ namespace MainApp
     {
         static void Main(string[] args)
         {
+            var myRSA = new RSA();
+            myRSA.GeneratePublicAndPrivateKeyData("Carlos");
+
+            Console.ReadKey();
         }
     }
 }
